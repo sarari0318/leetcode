8880d5b8e3fcc650986fac143d426294f6e1222d
Yuji Ito <ito0813sarari@gmail.com>
2020-10-10 18:51:41 +0900

    verified testcases

--- Heap/kth_largest_elements_8.py
+++ Heap/kth_largest_elements_8.py
@@ -17,3 +17,15 @@ class KthLargest:
             heapq.heapreplace(self.nums, val)
 
         return self.nums[0]
+
+    # testcase1: k=3, nums[4, 5, 8, 2], val=3
+    # nums[2, 4, 5, 8]
+    # heapq.heappop(nums) ⇨ nums[4, 5, 8]
+    # add(self.nums, 3) ⇨ nums[4, 5, 8]
+    # return 4 ⇨ Accepted!!
+
+    # testcase2: k=3, nums[4, 5, 8, 2], val=5
+    # nums[2, 4, 5, 8]
+    # heapq.heappop(nums) ⇨ nums[4, 5, 8]
+    # add(self.nums, 5) ⇨ nums[5, 5, 8]
+    # return 5 ⇨ Accepted!!
