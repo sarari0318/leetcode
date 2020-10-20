ba53ec65696d4e5954ec100806ddd004f3662d4c
Yuji Ito <ito0813sarari@gmail.com>
2020-10-20 14:01:28 +0900

    solve NumOfIsland17

---
+++ Graph/NumOfIsland17/number_of_islands_17.py
@@ -0,0 +1,30 @@
+class Solution:
+    def numIslands(self, grid):
+        
+        # return the number of islands
+        # pre: grid
+        # post: the number of islands
+        
+        if not grid:
+            return 0
+        
+        count = 0
+        
+        for hor in range(len(grid)):
+            for ver in range(len(grid[0])):
+                if grid[hor][ver] == '1':
+                    count += 1
+                    self.dfs(grid, hor, ver)
+                    
+        return count
+                
+    def dfs(self, grid, hor, ver):
+        if hor<0 or ver<0 or hor>=len(grid) or ver>= len(grid[0]) or grid[hor][ver]!='1':
+            return
+        
+        grid[hor][ver] = '#'
+        
+        self.dfs(grid, hor+1, ver)
+        self.dfs(grid, hor-1, ver)
+        self.dfs(grid, hor, ver+1)
+        self.dfs(grid, hor, ver-1)
