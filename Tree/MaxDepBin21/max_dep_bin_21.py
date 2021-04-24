fbc054413b84c676c13ccb4d48fda8407c68e7a1
Yuji Ito <ito0813sarari@gmail.com>
2021-04-24 20:39:40 +0900

    add comments to max_dep_bin

--- Tree/MaxDepBin21/max_dep_bin_21.py
+++ Tree/MaxDepBin21/max_dep_bin_21.py
@@ -1,7 +1,24 @@
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
 class Solution:
-	def maxDepth(self, root):
+	def maxDepth(self, root: TreeNode):
+		'''
+		This function returns the max depth of root(a binary tree).
+
+		Parameters
+		------------------------
+			root: TreeNode
+
+		Returns
+		------------------------
+			max depth of root: int
+		'''
+
 		root = TreeNode(root)
-		print(root.left)
 		if not root:
 			return 0
 		if root and root.left is None and root.right is None:
