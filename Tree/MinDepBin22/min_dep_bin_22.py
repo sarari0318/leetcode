0a1f897d3fc9a2b0140be521d411ab4ed3370968
Yuji Ito <ito0813sarari@gmail.com>
2021-04-24 20:44:38 +0900

    add comments to min_dep_bin

--- Tree/MinDepBin22/min_dep_bin_22.py
+++ Tree/MinDepBin22/min_dep_bin_22.py
@@ -1,16 +1,34 @@
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def minDepth(self, root: TreeNode) -> int:
+        '''
+        This function returns the min depth of root(a binary tree).
+
+        Parameters
+        ------------------------
+            root: TreeNode
+
+        Returns
+        ------------------------
+            min depth of root: int
+
+        '''
 
         if not root:
             return 0
-
         if root and root.left is None and root.right is None:
             return 1
-
+        # if there is only left
         if root.left and not root.right:
+            # validate only left
             return 1 + self.minDepth(root.left)
-
+        # if there is only right
         if not root.left and root.right:
+            # validate only right
             return 1 + self.minDepth(root.right)
 
         return 1 + min(self.minDepth(root.left), self.minDepth(root.right))
