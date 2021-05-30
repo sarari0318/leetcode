3b1d69c5aa0e0c45a0eb65b01f2d15d5d81a6929
Yuji Ito <ito0813sarari@gmail.com>
2021-05-30 10:49:36 +0900

    remove MaxDepBin21 directory

--- MaxDepBin21/max_dep_bin_21.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-	def maxDepth(self, root):
-		root = TreeNode(root)
-		print(root.left)
-		if not root:
-			return 0
-		if root and root.left is None and root.right is None:
-			return 1
-		return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
